f9332b4f1f6e78887f32d5c83a8cb918448a437b
MMontgomeryTaggart <shah@IM-CAR314.local>
2018-03-06 21:44:27 -0700

    Appending eHostess path to system path, to try to prevent imports from breaking.

--- eHostess/__init__.py
+++ eHostess/__init__.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
 __version__ = "0.0.1"
+
+eHostessPath = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(eHostessPath)
--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     description='A toolkit for working with eHost, pyConText, and other annotation and NLP tools.',
     url='https://github.com/MMontgomeryTaggart/eHostess',
     author='Max Taggart',
+    author_email = "max.taggart@utah.edu",
     license='MIT',
-    packages=['eHostess']
+    packages=['eHostess'],
+    download_url = 'https://github.com/MMontgomeryTaggart/eHostess/archive/0.0.1.tar.gz'
 )
