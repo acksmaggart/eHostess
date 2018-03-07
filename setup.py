265231fa583eccc2146366361ee9f8a4f1d6e45d
MMontgomeryTaggart <shah@IM-CAR314.local>
2018-03-06 22:30:26 -0700

    Fixed the package directories.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ehostess',
-    version='0.0.3',
+    version='0.0.4',
     description='A toolkit for working with eHost, pyConText, and other annotation and NLP tools.',
     url='https://github.com/MMontgomeryTaggart/eHostess',
     author='Max Taggart',
@@ -11,16 +11,14 @@ setup(
     packages = ['eHostess', 
         'eHostess.Analysis',
         'eHostess.Annotations',
-        'eHostess.DevelopmentAids',
         'eHostess.eHostInterface',
         'eHostess.MongoDBInterface',
         'eHostess.NotePreprocessing',
         'eHostess.PyConTextInterface',
-        'eHostess.PyConTextInterface',
         'eHostess.PyConTextInterface.SentenceSplitters',
         'eHostess.Utilities'
         ],
-    package_dir={'eHostess.PyConTextInterface' : 'PyConTextInterface'},
+    package_dir={'eHostess.PyConTextInterface' : 'eHostess/PyConTextInterface'},
     package_data = {'eHostess.PyConTextInterface' : ['TargetsAndModifiers/*.tsv']},
-    download_url = 'https://github.com/MMontgomeryTaggart/eHostess/archive/0.0.3.tar.gz'
+    download_url = 'https://github.com/MMontgomeryTaggart/eHostess/archive/0.0.4.tar.gz'
 )
