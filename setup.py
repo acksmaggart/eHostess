eb8fec3f88b2e780f12caa20c3bbc54ffca0a133
MMontgomeryTaggart <shah@IM-CAR314.local>
2018-03-06 22:12:54 -0700

    Added packages to setup.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,25 @@ from setuptools import setup
 
 setup(
     name='ehostess',
-    version='0.0.1',
+    version='0.0.3',
     description='A toolkit for working with eHost, pyConText, and other annotation and NLP tools.',
     url='https://github.com/MMontgomeryTaggart/eHostess',
     author='Max Taggart',
     author_email = "max.taggart@utah.edu",
     license='MIT',
-    packages=['eHostess'],
-    download_url = 'https://github.com/MMontgomeryTaggart/eHostess/archive/0.0.1.tar.gz'
+    packages = ['eHostess', 
+        'eHostess.Analysis',
+        'eHostess.Annotations',
+        'eHostess.DevelopmentAids',
+        'eHostess.eHostInterface',
+        'eHostess.MongoDBInterface',
+        'eHostess.NotePreprocessing',
+        'eHostess.PyConTextInterface',
+        'eHostess.PyConTextInterface',
+        'eHostess.PyConTextInterface.SentenceSplitters',
+        'eHostess.Utilities'
+        ],
+    package_dir={'eHostess.PyConTextInterface' : 'PyConTextInterface'},
+    package_data = {'eHostess.PyConTextInterface' : ['TargetsAndModifiers/*.tsv']},
+    download_url = 'https://github.com/MMontgomeryTaggart/eHostess/archive/0.0.3.tar.gz'
 )
